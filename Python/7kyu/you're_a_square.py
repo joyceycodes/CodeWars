124b7b6571eeadd745c3552ec2002febb8edbeaa
Joyce Yu <joyceyu96@gmail.com>
2022-12-21 16:22:40 -0800

    completed 7kyu

---
+++ Python/7kyu/you're_a_square.py
@@ -0,0 +1,41 @@
+# A square of squares
+# You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+# However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+# Task
+# Given an integral number, determine if it's a square number:
+
+# In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+# The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+# Examples
+# -1  =>  false
+#  0  =>  true
+#  3  =>  false
+#  4  =>  true
+# 25  =>  true
+# 26  =>  false
+
+# isSquare (-1) // => false
+# isSquare   3  // => false
+# isSquare   4  // => true
+# isSquare  25  // => true
+# isSquare  26  // => false
+
+# my solution
+def is_square(n):    
+    if n < 0:
+        return False
+    if int(n**(1/2))**2 == n:
+        return True
+    else: 
+        return False
+
+
+# top solution
+import math
+def is_square(n):
+    return n > -1 and math.sqrt(n) % 1 == 0
+# using mod 1 will get the remainder, so if the remainder is 0, that means it's a whole number
