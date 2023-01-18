b94e2735d2a188c40a2b3ab8814003de770b7280
Joyce Yu <joyceyu96@gmail.com>
2023-01-18 13:17:08 -0800

    completed 7kyu

--- Python/6kyu/your_order_please.py
+++ Python/6kyu/your_order_please.py
@@ -23,3 +23,24 @@ def order(sentence):
         result.append(alphanum[i+1])
     
     return ' '.join(result)
+
+
+# top solution
+def order(sentence):
+    if not sentence:
+        return ""
+    result = []    #the list that will eventually become our sentence
+      
+    split_up = sentence.split() #the original sentence turned into a list
+  
+    for i in range(1,10):
+        for item in split_up:
+            if str(i) in item:
+                 result.append(item)    #adds them in numerical order since it cycles through i first
+  
+    return " ".join(result)
+
+# top solution
+def order(words):
+  return ' '.join(sorted(words.split(), key=lambda w:sorted(w)))
+
