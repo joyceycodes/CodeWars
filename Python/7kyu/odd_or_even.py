6e177c854f5eb8b81099ef5b517ffd4d1795de9c
Joyce Yu <joyceyu96@gmail.com>
2023-02-02 23:44:57 -0800

    completed 7kyu

---
+++ Python/7kyu/odd_or_even.py
@@ -0,0 +1,26 @@
+# Given a list of integers, determine whether the sum of its elements is odd or even.
+
+# Give your answer as a string matching "odd" or "even".
+
+# If the input array is empty consider it as: [0] (array with a zero).
+
+# Examples:
+# Input: [0]
+# Output: "even"
+
+# Input: [0, 1, 4]
+# Output: "odd"
+
+# Input: [0, -1, -5]
+# Output: "even"
+
+# my solution
+def odd_or_even(arr):
+    if sum(arr) % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+# top solution
+def oddOrEven(arr):
+    return 'even' if sum(arr) % 2 == 0 else 'odd'
