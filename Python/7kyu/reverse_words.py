6f284f855faaefd9acad747e82e96dfa6558df5a
Joyce Yu <joyceyu96@gmail.com>
2023-02-06 15:30:25 -0800

    completed 7kyu

---
+++ Python/7kyu/reverse_words.py
@@ -0,0 +1,9 @@
+# Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+# Examples
+# "This is an example!" ==> "sihT si na !elpmaxe"
+# "double  spaces"      ==> "elbuod  secaps"
+
+# top solution
+def reverse_words(str):
+    return ' '.join(s[::-1] for s in str.split(' '))
