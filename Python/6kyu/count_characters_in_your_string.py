cb929a434170de7e4895f6fe590aeef69bd603f3
Joyce Yu <joyceyu96@gmail.com>
2023-03-11 21:36:03 -0800

    completed 6kyu

---
+++ Python/6kyu/count_characters_in_your_string.py
@@ -0,0 +1,11 @@
+# The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+# What if the string is empty? Then the result should be empty object literal, {}.
+
+def count(string):
+    hash = {}
+    for e in string:
+        if e not in hash:
+            hash[e] = 0
+        hash[e]+=1
+    return hash
