81f1d66b60e7e9e21829d702867a149db6c8c6f5
Joyce Yu <joyceyu96@gmail.com>
2023-03-14 16:09:27 -0700

    revisited 6kyu

--- Python/6kyu/sort_the_odds.py
+++ Python/6kyu/sort_the_odds.py
@@ -5,6 +5,18 @@
 # [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
+def sort_array(source_array):
+    # parameter = array of ints
+    # return a sorted array, evens remain in the same place, odds are sorted in asc order
+
+    odds = list(filter(lambda x: x % 2 != 0, source_array))
+    odds.sort()
+    for i in range(len(source_array)):
+        if source_array[i] % 2 != 0:
+            source_array[i] = odds[0]
+            odds.pop(0)
+    return source_array
+
 # top solution
 def sort_array(arr):
     # getting all the odd numbers out of the list in reverse sort order
