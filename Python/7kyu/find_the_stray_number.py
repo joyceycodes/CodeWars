8776ee28d0a7cc2d2af53fb2c5407675fa0aea8e
Joyce Yu <joyceyu96@gmail.com>
2023-03-15 15:20:00 -0700

    completed 8kyu and 7kyu

---
+++ Python/7kyu/find_the_stray_number.py
@@ -0,0 +1,18 @@
+# You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+# Complete the method which accepts such an array, and returns that single different number.
+
+# The input array will always be valid! (odd-length >= 3)
+
+# Examples
+# [1, 1, 2] ==> 2
+# [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+def stray(arr):
+    values = set(arr) # getting the two values from the arr
+    for i in values:
+        if arr.count(i) == 1:
+            return i
+
+def stray(arr):
+    return min(arr, key=arr.count)
