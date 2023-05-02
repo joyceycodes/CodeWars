1f1eb7801cae775cbea4e37f7c77a9623cf3cbf6
Joyce Yu <joyceyu96@gmail.com>
2023-05-01 21:59:28 -0700

    completed 8kyu

--- JavaScript/8kyu/catYearsDogYears.js
+++ JavaScript/8kyu/catYearsDogYears.js
@@ -21,10 +21,10 @@ var humanYearsCatYearsDogYears = function(humanYears) {
     let catYears = 0
     let dogYears = 0
     for(let i = 1; i <= humanYears; i++){
-      if(i == 1){
+      if(i === 1){
         catYears += 15
         dogYears += 15
-      } else if(i == 2){
+      } else if(i === 2){
         catYears += 9
         dogYears += 9
       } else {
