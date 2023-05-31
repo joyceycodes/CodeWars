43844b305e06f6081559cb0d04a05a690f4c0ae9
Joyce Yu <joyceyu96@gmail.com>
2023-05-31 15:06:59 -0700

    completed 6kyu

--- Python/6kyu/give_me_a_diamond.py
+++ Python/6kyu/give_me_a_diamond.py
@@ -24,14 +24,47 @@
 
 # "  *\n ***\n*****\n ***\n  *\n"
 
+def diamond(n):
+    # Make some diamonds!
+    result = ""
+    
+    if n < 0 or n % 2 == 0:
+        return None
+    
+    # there will be n lines for a n sized diamond (we'll need to loop n times)
+    # we need to add spaces, *, and \n for every line
+    # i* 2 - 1 to get the odd amount of stars
+    for i in range(1,(n//2)+2):
+        row =''
+        row += ' ' * (((n+1)//2)-i)
+        row += '*' * abs((i*2) -1)
+        row += '\n'
+        print(int(n/2), row)
+        result += row
+        
+    for i in range(n//2,0,-1):
+        row =''
+        row += ' ' * (((n+1)//2)-i)
+        row += '*' * ((i*2) -1)
+        row += '\n'
+        print(i,row)
+        result += row
+
+    return result
+
 # top solution
 def diamond(n):
-    if n > 0 and n % 2 == 1:
-        diamond = ""
-        for i in range(n):
-            diamond += " " * abs((n/2) - i)
-            diamond += "*" * (n - abs((n-1) - 2 * i))
-            diamond += "\n"
-        return diamond
-    else:
+
+    if n < 0 or n % 2 == 0:
         return None
+    
+    result = "*" * n + "\n"
+    spaces = 1
+    n = n - 2
+    while n > 0:
+        current = " " * spaces + "*" * n + "\n"
+        spaces = spaces + 1
+        n = n - 2
+        result = current + result + current
+    
+    return result
