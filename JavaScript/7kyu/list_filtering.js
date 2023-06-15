b35148de32971bac8c80a7a9c7e45535c6f2de60
Joyce Yu <joyceyu96@gmail.com>
2023-06-15 13:37:56 -0700

    completed 6kyu

--- JavaScript/7kyu/list_filtering.js
+++ JavaScript/7kyu/list_filtering.js
@@ -6,7 +6,7 @@
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
 function filter_list(l) {
-    return l.filter((a) => (Number.isInteger(a) && a >= 0))
+    return l.filter((a) => (Number.isInteger(a)))
   }
 
 // top solution
