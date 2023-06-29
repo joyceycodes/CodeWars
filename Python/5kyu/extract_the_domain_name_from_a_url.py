9dc5effe7c75c5f10ed2935a33b3fbe82a17c880
Joyce Yu <joyceyu96@gmail.com>
2023-06-29 13:58:46 -0700

    completed 7kyu

--- Python/5kyu/extract_the_domain_name_from_a_url.py
+++ Python/5kyu/extract_the_domain_name_from_a_url.py
@@ -15,6 +15,12 @@ def domain_name(url):
     else:
         return url[0]
 
-# top solution
+# top solutions
 def domain_name(url):
     return url.split("//")[-1].split("www.")[-1].split(".")[0]
+
+def domain_name(url):
+	url = url.split('//')[-1]
+	url = url.split('/')[0]
+	url = url.split('.')[-2]
+	return url
