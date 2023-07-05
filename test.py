9a3cd88112813202383b855742d317354b4c3e2e
Joyce Yu <joyceyu96@gmail.com>
2023-07-04 23:15:07 -0700

    completed 8kyu

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import random
+import pprint
+import collections
+
 random.seed(1)  # Setting random number generator seed for repeatability
 
 # REMINDER TO SELF, CHANGING NUM_DRONES FROM 10000 TO 10 FOR NOW
@@ -6,50 +9,119 @@ NUM_DRONES = 10
 AIRSPACE_SIZE = 100  # Meters. # changed from 128000
 CONFLICT_RADIUS = 20  # Meters. #changed from 500
 
+pp = pprint.PrettyPrinter(indent=4)
 
-def count_conflicts(drones, conflict_radius):
-    # INPUT - drones = a list containing the [x,y] corrdinates of each drone
-    # INPUT - conflict_radius = int, representing the distance in meters each drone must be from one another
-    # RETURN - int, total number of drones that are in conflict (within the conflict radius of another drone)
 
-    # distance between 2 drones = d = sqrt(x^2 + y^2)
+def squared_distance(point1, point2):
+    dx = point1[0] - point2[0]
+    dy = point1[1] - point2[1]
+    return (dx**2 ) + (dy**2)
 
-    # if drone is in conflict, we don't need to revisit it
-    conflicted_drones = set()
+BT = collections.namedtuple("BT", ["value", "left", "right"])
+"""
+A Binary Tree (BT) with a node value, and left- and
+right-subtrees.
+"""
 
-    j = len(drones) - 1
-    i = j - 1
+def build_kdtree(points, depth = 0):
+    n = len(points)
+    if n <= 0:
+        return None
+    
+    axis = depth % 2 # there's only two axis, x and y
+    sorted_points = sorted(points, key=lambda point:point[axis])
 
-    while j > 0 and len(drones) >= 2:
-        x = abs(drones[j][0] - drones[i][0])
-        y = abs(drones[j][1] - drones[i][1])
-        distance = ((x**2 ) + (y**2)) 
+    return BT (
+        value = sorted_points[n//2], # root of the tree/subtree
+        left = build_kdtree(sorted_points[:n//2], depth + 1),
+        right = build_kdtree(sorted_points[n//2 + 1:], depth + 1)
+    )
 
-        if distance < (conflict_radius ** 2):
-            # adding to our list of conflicted_drones
-            conflicted_drones.add(tuple(drones[j]))
-            conflicted_drones.add(tuple(drones[i]))
+NNRecord = collections.namedtuple("NNRecord", ["point", "distance"])
+"""
+Used to keep track of the current best guess during a nearest
+neighbor search.
+"""
 
-            # swapping the elements so that drones[i] can be the second last item in the list
-            drones[i],drones[j-1] = drones[j-1],drones[i]
+def find_nearest_neighbor(*, tree, point):
+    """Find the nearest neighbor in a k-d tree for a given
+    point.
+    """
+    k = len(point)
 
-            # popping the last two items (conflicting drones) off the list
-            drones.pop()
-            drones.pop()
+    best = None
 
-            j = len(drones) - 1
-            i = j - 1
+    def search(*, tree, depth):
+        """Recursively search through the k-d tree to find the
+        nearest neighbor.
+        """
+        nonlocal best
 
-        # popping off the last drone if it had no conflicts
-        elif i == 0:
-            drones.pop()
-            j = len(drones) - 1
-            i = j - 1
-
+        if tree is None:
+            return
+        
+        distance = squared_distance(tree.value, point)
+        if best is None or distance < best.distance:
+            best = NNRecord(point=tree.value, distance=distance)
+        
+        axis = depth % k
+        diff = point[axis] - tree.value[axis]
+        if diff <= 0:
+            close, away = tree.left, tree.right
         else:
-            i -= 1
+            close, away = tree.right, tree.left
+        
+        search(tree=close, depth=depth+1)
+        if diff**2 < best.distance:
+            search(tree=away, depth=depth+1)
+    
+    search(tree=tree, depth=0)
+    print(best.point)
+    return best.point
+    
+# def count_conflicts(drones, conflict_radius):
+#     conflicted_drones = set()
+#     for point in drones:
+#         nearest_neighbor = find_nearest_neighbor(tree=tree, point=point)
+#         print(nearest_neighbor)
+#         print(point)
+#         if squared_distance(point, nearest_neighbor) < conflict_radius:
+#             conflicted_drones.add(point)
+#             conflicted_drones.add(nearest_neighbor)
+#     return len(conflicted_drones)
 
-    return len(conflicted_drones)  
+    # j = len(drones) - 1
+    # i = j - 1
+
+    # while j > 0 and len(drones) >= 2:
+    #     x = abs(drones[j][0] - drones[i][0])
+    #     y = abs(drones[j][1] - drones[i][1])
+    #     distance = ((x**2 ) + (y**2)) 
+
+    #     if distance < (conflict_radius ** 2):
+    #         # adding to our list of conflicted_drones
+    #         conflicted_drones.add(tuple(drones[j]))
+    #         conflicted_drones.add(tuple(drones[i]))
+
+    #         # swapping the elements so that drones[i] can be the second last item in the list
+    #         drones[i],drones[j-1] = drones[j-1],drones[i]
+
+    #         # popping the last two items (conflicting drones) off the list
+    #         drones.pop()
+    #         drones.pop()
+
+    #         j = len(drones) - 1
+    #         i = j - 1
+
+    #     # popping off the last drone if it had no conflicts
+    #     elif i == 0:
+    #         drones.pop()
+    #         j = len(drones) - 1
+    #         i = j - 1
+
+    #     else:
+    #         i -= 1
+
 
     # for i in range(len(drones)):
     #     for j in range(i+1,len(drones)):
@@ -57,41 +129,23 @@ def count_conflicts(drones, conflict_radius):
     #         y = abs(drones[i][1] - drones[j][1])
     #         d = ((x**2 ) + (y**2)) ** (1/2)
     #         print(drones[i] ,drones[j], d)
-    #         count += 1
     #         if d < conflict_radius:
-    #             conflicts.add(tuple(drones[i]))
-    #             conflicts.add(tuple(drones[j]))
+    #             conflicted_drones.add(tuple(drones[i]))
+    #             conflicted_drones.add(tuple(drones[j]))
+    # print(conflicted_drones)
+    return len(conflicted_drones)  
+
 def gen_coord():
     # all drones will in the drones list will be within the airspace
     return int(random.random() * AIRSPACE_SIZE)
 
 positions = [[gen_coord(), gen_coord()] for i in range(NUM_DRONES)]
-conflicts = count_conflicts(positions, CONFLICT_RADIUS)
-print("Drones in conflict: {}".format(conflicts))
+# pp.pprint(build_kdtree(positions))
+tree = build_kdtree(positions)
+# conflicts = count_conflicts(positions, CONFLICT_RADIUS)
+# print("Drones in conflict: {}".format(conflicts))
 
-# trying merge sort
-# def partition(drones, conflict_radius, left=None, right=None):
-#     pivot = drones[right]
-#     star = left - 1
-
-#     for i in range(left,right):
-#         x = abs(drones[i][0] - drones[j][0])
-#         y = abs(drones[i][1] - drones[j][1])
-#         d = ((x**2 ) + (y**2)) ** (1/2)
-#         print(d)
-        
-
-
-# def count_conflicts(drones, conflict_radius, left=None, right=None):
-#     if left == None and right == None:
-#         left = 0
-#         right = len(drones) -1
-    
-#     if left >= right or left < 0:
-#         return
-
-#     p = partition(drones, conflict_radius, left, right)
-#     count_conflicts(drones, left, p-1)
-#     count_conflicts(drones, p+1, right)
+print(find_nearest_neighbor(tree=tree, point=[9,2]))
+# print(len(NNRecord))
 
     
