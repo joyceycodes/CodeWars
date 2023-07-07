daec5cb5d3f9d5733e8a43f129a7bb821dfd58a2
Joyce Yu <joyceyu96@gmail.com>
2023-07-06 19:51:37 -0700

    completed 6kyu

--- Python/7kyu/remove_duplicate_words.py
+++ Python/7kyu/remove_duplicate_words.py
@@ -11,8 +11,12 @@
 # 'alpha beta gamma delta'
 
 def remove_duplicate_words(s):
-    count = {}
+    count = []
     for i in s.split():
         if i not in count:
-            count[i] = 1
-    return " ".join([i for i in count.keys()])
+            count.append(i)
+    return " ".join(count)
+
+# top solution
+def remove_duplicate_words(s):
+    return ' '.join(dict.fromkeys(s.split()))
