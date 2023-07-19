7dfacab37b0e62c2dd6165601976ad1cc978a56a
Joyce Yu <joyceyu96@gmail.com>
2023-07-19 13:33:49 -0700

    completed 6kyu

--- Python/8kyu/is_it_even.py
+++ Python/8kyu/is_it_even.py
@@ -1,6 +1,15 @@
-def count_conflicts(drones, conflict_radius):
-    for i in range(len(drones)):
-        for j in range(i+1,len(drones)):
-            print(i,j)
+def is_merge(s, part1, part2):
+    queue = [(s,part1,part2)]
+    while queue:
+        str, p1, p2 = queue.pop()            
+        if str:
+            if p1 and str[0] == p1[0]:
+                queue.append((str[1:], p1[1:], p2))
+            if p2 and str[0] == p2[0]:
+                queue.append((str[1:], p1, p2[1:]))
+        else:
+            if not p1 and not p2:
+                return True
+    return False
             
-count_conflicts([1,2,3,4,5],10)
+print(is_merge('Bananas from Bahamas', 'Bahas', 'Bananas from am'))
