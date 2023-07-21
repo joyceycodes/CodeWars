7b3783383b6992a8aa15f83b80de38943bd6527e
Joyce Yu <joyceyu96@gmail.com>
2023-07-21 13:21:39 -0700

    completed 7kkyu

---
+++ Python/7kyu/greatest_common_divisor.py
@@ -0,0 +1,15 @@
+# Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+# The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+def mygcd(x,y):
+    #GOOD LUCK
+    while y:
+        x,y=y,x%y
+    return x
+
+def mygcd(x,y):
+    if x%y ==0:
+        return y
+    else:
+        return mygcd(y,x%y)
