368c3c501be48f73fe5bd045dd397b82f73d1636
Joyce Yu <joyceyu96@gmail.com>
2023-07-22 21:45:24 -0700

    completed 7kyu

--- Python/8kyu/is_it_even.py
+++ Python/8kyu/is_it_even.py
@@ -1,7 +1,19 @@
-def mygcd(x,y):
-    if x%y ==0:
-        return y
-    else:
-        return mygcd(y,x%y)
+def topKFrequent(nums,k):
+    count = {}
+    bucket = [[] for i in range(len(nums)+1)]
+    
+    for i in nums:
+        # adds 1 to the value at count[n], also sets count[n] to 0 if count[n] does not exist yet
+        count[i] = 1 + count.get(i,0)
+
+    for k,v in count.items():
+        bucket[v].append(k)
+
+    result = []
+    for i in range(len(bucket)-1, 0, -1):
+        for n in bucket[i]:
+            result.append(n)
+            if len(result) == k:
+                return result
             
-print(mygcd(96,360))
+print(topKFrequent([1,1,1,2,2,3],2))
--- Python/Incomplete/greatest_common_divisor.py
+++ Python/Incomplete/greatest_common_divisor.py
@@ -2,6 +2,7 @@
 
 # The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
 
+# 7kyu
 def mygcd(x,y):
     while y:
         x,y=y,x%y
