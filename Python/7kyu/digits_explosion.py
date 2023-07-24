1a1fc997cc569c916a9e7c2a52eed196633402d5
Joyce Yu <joyceyu96@gmail.com>
2023-07-23 21:53:13 -0700

    completed 7kyu

---
+++ Python/7kyu/digits_explosion.py
@@ -0,0 +1,14 @@
+# Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+# Examples
+# explode("312")
+# should return :
+
+# "333122"
+# explode("102269")
+# should return :
+
+# "12222666666999999999"
+
+def explode(s):
+    return ''.join([i* int(i) for i in s])
