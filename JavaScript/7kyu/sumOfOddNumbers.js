7c0cafcccddb071fe3ea8eba29361984ea246046
Joyce Yu <joyceyu96@gmail.com>
2023-08-08 11:19:54 -0700

    completed 7kyu

---
+++ JavaScript/7kyu/sumOfOddNumbers.js
@@ -0,0 +1,31 @@
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+function rowSumOddNumbers(n) {
+	let odds = []
+  curr_num = 1
+  for(let i = 0; i < n+1; i++){
+    let row = []
+    while (row.length < i){
+      row.push(curr_num)
+      curr_num += 2
+    }
+    odds.push(row)
+  }
+  return (odds[n].reduce((a,e)=> a + e, 0))
+}
+
+
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
+  }
