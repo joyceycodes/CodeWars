34977bb84025c598c0ac0b9884fab35131bb1444
Joyce Yu <joyceyu96@gmail.com>
2023-08-10 11:18:16 -0700

    completed 7kyu

--- Python/Incomplete/find_the_missing_term_in_an_arithmetic_progression.py
+++ Python/Incomplete/find_the_missing_term_in_an_arithmetic_progression.py
@@ -1,5 +1,17 @@
 # https://www.codewars.com/kata/52de553ebb55d1fca3000371/
 
-def find_missing(sequence):
-    t = sequence
-    return (t[0] + t[-1]) * (len(t) + 1) / 2 - sum(t)
+def find_missing(nums):
+        nums.sort()
+        longest_seq = 1
+        curr_seq = 1
+        for i in range(1,len(nums)):
+            # start of seq
+            if nums[i] - nums[i-1] == 1:
+                curr_seq += 1
+                while curr_seq+i < len(nums) and nums[i] + curr_seq == nums[i+curr_seq]:
+                    curr_seq += 1
+                longest_seq = max(longest_seq, curr_seq) 
+            curr_seq = 1
+        return longest_seq 
+    
+print(find_missing([100,4,200,1,3,2]))
