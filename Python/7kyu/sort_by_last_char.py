f09ab14dc7cf23a9f4c72f2214d72cfaa2f8a13c
Joyce Yu <joyceyu96@gmail.com>
2023-11-03 13:44:33 -0700

    Created sort_by_last_char.py

---
+++ Python/7kyu/sort_by_last_char.py
@@ -0,0 +1,8 @@
+# Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+# If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+# All inputs will be valid.
+
+def last(s):
+    return sorted(s.split(' '), key=lambda x:x[-1])
