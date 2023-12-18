8a0f4b36a1d1cf98f7dd3c521480083dcc955b6c
Joyce <100826052+joyceycodes@users.noreply.github.com>
2023-12-17 22:11:58 -0800

    Create reverseANumber.js

---
+++ JavaScript/7kyu/reverseANumber.js
@@ -0,0 +1,26 @@
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+// Examples
+//  123 ->  321
+// -456 -> -654
+// 1000 ->    1
+
+function reverseNumber(n) {
+  if (n > 0){
+    n = n.toString()
+    return parseInt(n.split('').reverse().join(''))
+  } else {
+    n = n.toString()
+    return parseInt(n.split('').reverse().join('')) * -1
+  }
+}
+
+function reverseNumber(n) {
+  let isNegative = n < 0;  
+  let reverseAsString = Math.abs(n).toString().split('').reverse().join('');
+  let result = Number(reverseAsString);
+  
+  return isNegative ? -result : result;
+}
